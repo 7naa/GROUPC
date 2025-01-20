9d46e36f581534cd9f932f7da46062023515ca6e
aunishukri <b022210115@student.utem.edu.my>
2025-01-20 21:16:19 +0800

    change express

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 const port = process.env.PORT || 4000;
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const rateLimit = require('express-rate-limit');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // Middleware to parse JSON in request body
