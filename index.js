74c3ee05596c75ecf4c20741c69747368ac24d8a
aunishukri <b022210115@student.utem.edu.my>
2025-01-20 23:20:12 +0800

    sentence rate

--- index.js
+++ index.js
@@ -44,11 +44,11 @@ function verifyAdmin(req, res, next) {
 
 // Rate limiting middleware
 const loginLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 15 minutes
-  max: 2, // Limit each IP to 5 login requests per `window` (15 minutes)
+  windowMs: 1 * 60 * 1000, // 5 minutes
+  max: 2, // Limit each IP to 5 login requests per `window` (5 minutes)
   message: {
     status: 429,
-    message: "Too many login attempts from this IP. Please try again after 15 minutes."
+    message: "Too many login attempts from this IP. Please try again after 5 minutes."
   },
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
